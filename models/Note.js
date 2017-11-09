908f31cf4815416cda87e90e0ebc95a21320e7ff
dcgutierrez93 <dcgutierrez93@yahoo.com>
2017-11-09 11:51:59 -0600

    Added Note Schema

--- models/Note.js
+++ models/Note.js
@@ -3,3 +3,22 @@
 // Note Model
 
 // =============================================================================
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+
+var noteSchema = new Schema({
+    // The headline is the article associate with the note
+    _headlineId: {
+        type: Schema.Types.ObjectId,
+        ref : "Headline"
+    },
+    // date is just a string
+    date: String,
+    // as is the noteText
+    noteText: String
+});
+
+// Create the Note model using the noteSchema
+var Note = mongoose.model("Note", noteSchema);
+
+module.exports = Note;
